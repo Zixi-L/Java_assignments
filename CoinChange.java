0779af588b7fc49eb4d63ad76136320b5a477851
laibaobaby <49736523+laibaobaby@users.noreply.github.com>
2019-12-11 23:18:25 -0800

    Add files via upload

---
+++ CoinChange.java
@@ -0,0 +1,125 @@
+package lab2;
+import java.util.Scanner;
+public class CoinChange
+{
+	public static void main(String[] args) 
+	{
+		{
+			   {
+			       int total,quarter,dime,nickel;
+			       int potatoQuantity=1,cookieQuantity=5,candyQuantity=5; //quantity 
+			       int potatoPrice=125,cookiePrice=85,candyPrice=95; //price
+			       int change=0;
+			       Scanner sc=new Scanner(System.in);
+			       while (true)
+			       {
+			       System.out.println("*****WELCOME*****");
+			       System.out.println("Potato Chips $1.25");
+			       System.out.println("Cookies $0.85");
+			       System.out.println("Candies $0.95");
+			       System.out.println("Please enter quarter dime or nickel (put space between)\n");
+			       quarter=sc.nextInt();
+			       dime=sc.nextInt();
+			       nickel=sc.nextInt();
+			       total=quarter*25+dime*10+nickel*5;
+			     
+			      
+			           System.out.print("Enter 1 for patato chips\n");
+			           System.out.print("Enter 2 for cookies\n");
+			           System.out.print("Enter 3 for candy\n");
+			           int n=sc.nextInt();
+			           switch(n)
+			           {
+			               case 1:
+			            	   if(potatoQuantity > 0) 
+			            	   {
+			            		   if(total<potatoPrice)
+			            		   {
+			            			   System.out.print("Not enough money\n");
+			            		   }  
+			            		   else if(total>=potatoPrice) 
+			               			{		          
+			               			total = total - potatoPrice;
+			               			quarter = total / 25;
+			               			total = total % 25;
+			               			dime = total / 10;
+			               			total = total % 10 ;
+			               			nickel = total / 5;
+			               			System.out.println("Your change is: " + "quarter:" + quarter + "  dime:" + dime + "  nickel: " + nickel );
+			               			System.out.println("Please take your food");
+			               			potatoQuantity--;
+			               			}
+			            	   }
+			            	   else 
+			            	   {
+			            		   System.out.println("Sorry, potato chips is sold out. Please choose another food.");
+			            		   break;
+			            	   }
+			               break;
+			              
+			                   case 2:
+			                	   if(cookieQuantity>0)
+			                	   {
+			                	   if(total < cookiePrice)
+			                	   {
+			                   System.out.print("Not enough money\n");
+			                	   }  
+			                   else if (total >= cookiePrice)
+			               
+			                   		{
+			                	    total = (total - cookiePrice);
+			               			quarter = total / 25;
+			               			total = total % 25;
+			               			dime = total / 10;
+			               			total = total %10 ;
+			               			nickel = total / 5;
+			               			System.out.println("Your change is: " + "quarter: " + quarter + "  dime: " + dime + "  nickel: " + nickel );
+			               			System.out.println("Please take your food");
+				               		cookieQuantity--;
+			                   }
+			                	   }
+			                	   else
+			                	   {
+			                		   System.out.println("Sorry, cookie is sold out. Please choose another food.");
+			                		  break;
+			                	   }
+			               break;
+			              
+			              
+			              
+			                   case 3:
+			                	   if(candyQuantity>0)
+			                	   {
+			                	   if(total<candyPrice)
+			                	   {
+			                   System.out.print("Not enough money\n");
+			                	   }  
+			                   else if (total>=candyPrice)
+			                   {
+			                	   	total = total - candyPrice;
+			               			quarter = total / 25;
+			               			total = total % 25;
+			               			dime = total / 10;
+			               			total = total %10 ;
+			               			nickel = total / 5;
+			               			System.out.println("Your change is: " + "quarter: " + quarter + "  dime: " + dime + "  nickel: " + nickel );
+			               			System.out.println("Please take your food");
+				               		candyQuantity--;
+			                   }
+			                	   }
+			                	   else 
+			                	   {
+			                		   System.out.println("Sorry, candy is sold out. Please choose another food.");
+			                	   break;
+			                	   }
+			               break;
+			              
+			               default:return;      
+			               
+			           }
+			          
+			       }
+			   	}
+			}
+	}
+}
